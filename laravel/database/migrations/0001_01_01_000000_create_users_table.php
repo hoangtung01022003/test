c7623d76e201a78732c5a7616ecb0dc1e31c8cec
nggialam13 <gialam130704@gmail.com>
2026-04-11 21:54:42 +0700

    COMMIT: setup database users hoan chinh

--- laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,15 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+           
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('avatar')->nullable();
+
+            $table->tinyInteger('role')->default(0);
+
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
