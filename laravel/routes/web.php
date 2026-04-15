ecb5caba6c2b0427a0431633185155acf8d55915
pdHanh <pdhanh.12a3.2020@gmail.com>
2026-04-15 23:36:57 +0700

    create routre login

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+// Điều hướng login
+Route::get('login', [UserController::class, 'login'])->name('login');
+Route::post('login', [UserController::class, 'authUser'])->name('user.authUser');
