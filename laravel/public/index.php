654dcebd5ade66a9f113e6224d2e34504cf890b4
Hoàng Văn Tùng <101704209+hoangtung01022003@users.noreply.github.com>
2026-04-16 20:05:50 +0700

    Remove commented out debug statement

--- laravel/public/index.php
+++ laravel/public/index.php
@@ -13,6 +13,12 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+
+
+
+
+
+//dd
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
